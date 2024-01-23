66e3c0291261788df300deb403e1f83fe4b17fdc
inskim4330 <inskim4330@gmail.com>
2024-01-23 23:26:06 +0900

    Floating-Point Literals

--- cpplab.cpp
+++ cpplab.cpp
@@ -1,20 +1,10 @@
-// cpplab.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <cstdio>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    double an = 6.0221409e23;
+    printf("Avogadro's Number: %le %lf %lg \n", an, an, an);
+    float hp = 9.75;
+    printf("Hogwarts' Platform: %e %f %g \n", hp, hp, hp);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
